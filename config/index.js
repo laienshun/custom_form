e83be3bdb746f750e503bc316613e9d730a29a35
laienshun <768595267@qq.com>
2019-03-06 11:58:19 +0800

    修改bug

--- config/index.js
+++ config/index.js
@@ -38,11 +38,11 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../static/index.html'),
+    index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../static'),
-    assetsSubDirectory: 'client_static',
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
     /**
